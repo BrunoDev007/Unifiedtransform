31c0035b32735cd05456790408ca205f023b7bd1
BrunoDev007 <bruno_xr@outlook.com>
2025-03-06 08:10:59 -0300

    Tradução do Projeto PT-BR

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As linhas de idioma a seguir são usadas durante a autenticação para várias
+    | mensagens que precisamos exibir ao usuário. Você pode modificar essas linhas
+    | conforme os requisitos do seu aplicativo.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+];
---
+++ resources/lang/pt-BR/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Paginação
+    |--------------------------------------------------------------------------
+    |
+    | As linhas de idioma a seguir são usadas pela biblioteca de paginação para
+    | construir os links de paginação simples. Sinta-se à vontade para alterá-las
+    | para qualquer coisa que desejar para melhor se adequar ao seu aplicativo.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
